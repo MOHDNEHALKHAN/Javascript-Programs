d09bae19ae41f3f7f0b58cf579e786c36e041ff2
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-12-03 09:52:39 +0530

    added exercise number 3

---
+++ Exercise3.js
@@ -0,0 +1,5 @@
+/*Convert the age of dog age into human age */
+
+let dogAge = prompt("Write the age of your dog"); // prompt only 
+let humanAge = (dogAge - 2) * 4 + 21;
+alert("Your dog is " + humanAge + " years old");
