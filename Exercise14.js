82bd7f54542056661819736af0f25489d658edea
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-12-14 10:51:37 +0530

    Added exercise number 14

---
+++ Exercise14.js
@@ -0,0 +1,28 @@
+/* Fizz Buzz problem using while loop */
+
+var output = [];
+var count = 1;
+
+function fizzBuzz(){
+
+    while(count <= 100)
+        {
+    if(count % 3 === 0 && count % 5  === 0){
+        output.push("FizzBuzz");
+    }
+     else if(count % 3 === 0){
+        output.push("Fizz")
+    }
+         else if(count % 5 === 0){
+             output.push("Buzz");
+         }
+     
+     else{
+         output.push(count);
+    }
+    
+    count++;
+        }
+    console.log(output);
+}
+console.log(fizzBuzz());
