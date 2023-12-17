a8f38638fc45c13141650850ffedc412688b879b
MOHD NEHAL KHAN <nehalk482@gmail.com>
2023-12-17 11:33:31 +0530

    Added exercise 17

---
+++ Exercise17.js
@@ -0,0 +1,25 @@
+/*To pass the function in a function  */
+
+// for addition
+function add(num1,num2){
+    return num1 + num2 ;
+    }
+    // for subtraction
+    function subtract(num1,num2){
+    return num1 - num2 ;
+    }
+    // for multiplication
+    function multiply(num1,num2){
+    return num1 * num2 ;
+    }
+    // for division
+    function divide(num1,num2){
+    return num1 / num2 ;
+    }
+    //function to pass the other function
+    
+    function Calculator(num1,num2,opr){
+    return opr(num1,num2);
+    }
+    
+    console.log(Calculator(2,3,multiply));
